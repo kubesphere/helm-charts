083d0b2e17bf3db70bf8ae683df956a1015ff6d1
Hongliang Wang <hongliangwang@yunify.com>
2020-11-04 13:23:39 +0800

    Feature/gitee (#108)
    * enable auto sync to git mirror (Gitee)
    
    Signed-off-by: Hongliang Wang <hongliangwang@yunify.com>
    
    * remove src and build dirs
    
    Signed-off-by: Hongliang Wang <hongliangwang@yunify.com>

--- build.sh
+++ build.sh
@@ -134,12 +134,29 @@ pushUpdates() {
   git push origin HEAD:gh-pages
 }
 
+pushRemote() {
+  ensureVars GITEE_USER GITEE_TOKEN
+  git remote add gitee https://$GITEE_USER:$GITEE_TOKEN@gitee.com/kubesphere/helm-charts.git
+  git push gitee HEAD:${1:-master} ${@:2}
+  git remote rm gitee
+}
+
+adjustRemote() {
+  ensureVars BASE_URL BASE_URL_CN
+  rm -rf src/ build/
+  files="$(find . -type f -name index.html -or -name index.yaml)"
+  sed -i "s#$BASE_URL#$BASE_URL_CN#g" $files
+  git add $files
+  git commit -m "Update" --author="KubeSphere CI Bot <ks-ci-bot@users.noreply.github.com>"
+}
+
 verify() {
   prepareHelm
   updateRepos
 }
 
 deploy() {
+  pushRemote
   verify
 
   [ -z "$updatedRepos" ] || {
@@ -150,6 +167,8 @@ deploy() {
       mkdir -p $repo && mv $buildDir/$repo/* $repo/ && git add $repo
     done
     pushUpdates
+    adjustRemote
+    pushRemote gh-pages -f
   }
 }
 
